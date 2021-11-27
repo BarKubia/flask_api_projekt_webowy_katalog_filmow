1faebb57c2d2e387d356c33529d8dd655fb25ae6
Bartosz Kubiak <barkubia1@gmail.com>
2021-11-27 14:18:06 +0100

    Fix project

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    selected_list = request.args.get('list_type', "popular")
+    selected_list = request.args.get("list_type", "popular")
     movies = tmdb_client.get_movies(how_many=8, list_type=selected_list)
     return render_template("homepage.html", movies=movies, current_list=selected_list)
 
@@ -23,5 +23,5 @@ def movie_details(movie_id):
     details = tmdb_client.get_single_movie(movie_id)
     cast = tmdb_client.get_cast(movie_id, how_many2)
     movie_images = tmdb_client.get_movie_images(movie_id)
-    selected_backdrop = random.choice(movie_images['backdrops'])
+    selected_backdrop = random.choice(movie_images["backdrops"])
     return render_template("movie_details.html", movie=details, cast=cast, selected_backdrop=selected_backdrop)
--- tmdb_client.py
+++ tmdb_client.py
@@ -1,14 +1,16 @@
 import requests
 api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MjUxMDZmMjM5NDhjMWQzNDBiNjcxYjUxMWYxNjExNyIsInN1YiI6IjYxOWU0NWEzMzEwMzI1MDA0MzZiN2JiNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xhHIchVIGim0YU97QwzCT5s26JNqGViD7moe15F8Py8"
 
-
-def get_popular_movies():
-    endpoint = "https://api.themoviedb.org/3/movie/popular"
-    
+def headers_f(endpoint_f):
     headers = {
         "Authorization": f"Bearer {api_token}"
     }
-    response = requests.get(endpoint, headers=headers)
+    response = requests.get(endpoint_f, headers=headers)
+    return response
+
+def get_popular_movies():
+    endpoint = "https://api.themoviedb.org/3/movie/popular"
+    response=headers_f(endpoint)
     return response.json()
 
 def get_poster_url(poster_api_path, size="w342"):
@@ -16,25 +18,20 @@ def get_poster_url(poster_api_path, size="w342"):
     return f"{base_url}{size}/{poster_api_path}"
 
 def get_movies(how_many, list_type):
-    if list_type!="top_rated" and list_type!="upcoming" and list_type!="popular" and list_type!="now_playing":
+    
+    if list_type not in ["top_rated", "upcoming", "popular", "now_playing"]:
         list_type="popular"
     data=get_movies_list(list_type)
     return data["results"][:how_many]
 
 def get_single_movie(movie_id):
     endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
-    headers = {
-        "Authorization": f"Bearer {api_token}"
-    }
-    response = requests.get(endpoint, headers=headers)
+    response=headers_f(endpoint)
     return response.json()
 
 def get_single_movie_cast(movie_id):
     endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
-    headers = {
-        "Authorization": f"Bearer {api_token}"
-    }
-    response = requests.get(endpoint, headers=headers)
+    response=headers_f(endpoint)
     return response.json()["cast"]
 
 def get_cast(movie_id,how_many2):
@@ -43,17 +40,11 @@ def get_cast(movie_id,how_many2):
 
 def get_movie_images(movie_id):
     endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/images"
-    headers = {
-        "Authorization": f"Bearer {api_token}"
-    }
-    response = requests.get(endpoint, headers=headers)
+    response=headers_f(endpoint)
     return response.json()
 
 def get_movies_list(list_type):
     endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
-    headers = {
-        "Authorization": f"Bearer {api_token}"
-    }
-    response = requests.get(endpoint, headers=headers)
+    response=headers_f(endpoint)
     response.raise_for_status()
     return response.json()
